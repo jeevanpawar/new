23d605fed2e144c700dc8c3b7b762040862f23cd
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-07-23 12:12:23 +0530

    sdf

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+
+<body>
+<div class="container">
+  <div class="header">
+  <div class="logo">
+  <img src="./images/logo.png" />
+  </div>
+  <div class="heading">
+  	<h3><font color="#CC0000" size="+2">N</font>AVRATNA <font color="#CC0000" size="+2">G</font>EMOLOGICAL <font color="#CC0000" size="+2">L</font>ABORATORY &#0153;</h3>
+	<p>CERTIFICATION OF GEMSTONES & DIAMONDS GRADING LABOROTARY</p>
+	<p style="color:#ffffff;"><font style="background-color:#990033">AN ISO 9001-2008 CERTIFIED LAB</font></p>
+	<h6>A LAB TRADE MARK UNDER GOVERNMENT OF INDIA(REGISTRY DEPARTMENT)</h6>
+	<P><font style="background-color:#990033; color:#FFFFFF">1st GEMOLOGICAL LAB IN NORTH MAHARASTRA</font></P>
+  </div>
+  </div>
+  
+  <div class="content">
+  	<h3>GEMS IDENTIFICATION REPORT</h3>
+	<table class="data">
+	<tr><td>REPORT NO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>CARAT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>3.17CT </td></tr>
+	<tr><td>COLOR&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>LCY</td></tr>
+	<tr><td>CUT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>CLARITY(GRADE)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>REFRACTIVE INDEX(R.I)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>SPECIFIC GRAVITY(S.G)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>HARDNESS(H)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>TRANSPARANCY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>MINING&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>94100/NRB</td></tr>
+	<tr><td>LUSTER</td><td>94100/NRB</td></tr>
+	<tr><td>INCLUSION</td><td>94100/NRB</td></tr>
+	<tr><td>COMMENTS</td><td><></td></tr>
+	</table>
+	
+  
+  
+  <div class="customer">
+  <div class="custlogo">
+	<img src="./images/cmpny.jpg" />
+  </div>
+  <div class="custitem">
+	<img src="./images/pachu.jpg" />
+	<p>CHIFF GEMOLOGIST(N.G.L)</p>
+  </div>
+  </div>
+  </div><!--end of content-->
+  <div class="footer">
+  <p>IDENTIFICATION:-<font color="#CC0000" > ****NATURAL YELLO SAPPHIRE(PUKHRAJ)****</font></p>
+  </div>
+  
+</div>
+</body>
+</html>
+<?php
+$htmlcontent=ob_get_clean();
+include("dompdf/dompdf_config.inc.php");
+  $htmlcontent = stripslashes($htmlcontent);
+  $dompdf = new DOMPDF();
+  $dompdf->load_html($htmlcontent);
+  $dompdf->set_paper("folio", "portrait");
+  $dompdf->render();
+  $dompdf->stream($filename, array("Attachment" => false));	
+  exit(0);
+?>
